7903ba378a79597525f4542aa2c6d3772f349c7f
MCPolichette <mark@polichette.com>
2018-10-24 10:05:58 -0600

    deleted files, minor adjustments.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import PortfolioCard from "./components/PortfolioCard";
 import ProjectCard from "./components/ProjectCard";
 import "./App.css";
 import pieces from "./pieces.json";
 class App extends Component {
-  state = { pieces: pieces };
+  state = {
+    pieces: pieces
+  };
   render() {
     return (
       <div className="App">
@@ -18,23 +19,24 @@ class App extends Component {
             <div className="col s12">
               <p className="App-intro">
                 Having worked in a few different industries, I bring a new perspective to the table.  I’ve worked labor, retail, manufacturing, and in a laboratory.  Because of  those job’s I’ve been able to take lessons from one industry and apply them elsewhere to improve operations.   I like to take on new challenges, and learn new ways to tackle problems.
-  I am new to programming, but I have finished a course through the University of Utah  working with Javascript, Node, Express, React, Mongodb, SQL and more.
-  
+              </p>
+              <p>
+                I am new to programming, but I have finished a course through the University of Utah  working with Javascript, Node, Express, React, Mongodb, SQL and more.
               </p>
             </div>
           </div>
           <div className="row">
             <div className="col s12 center">
               <div>
-                {this.state.pieces.map(pieces => (
+                {this.state.pieces.map(piece => (
                   <ProjectCard
-                    name={pieces.name}
-                    key={pieces.key}
-                    image={pieces.image}
-                    link={pieces.link}
-                    github={pieces.github}
-                    tech={pieces.tech}
-                    description={pieces.description}
+                    name={piece.name}
+                    key={piece.key}
+                    image={piece.image}
+                    link={piece.link}
+                    github={piece.github}
+                    tech={piece.tech}
+                    description={piece.description}
                   />
                 ))}
               </div>
--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -3,7 +3,7 @@ import "./ProjectCard.css";
 
 const ProjectCard = props => (
   <div className="card">
-    <h5 className="card-topper">{props.name}</h5>
+    <h5 className=" activator card-topper">{props.name}</h5>
     <div className="card-image waves-effect waves-block waves-light">
 
       <img className="activator" src={props.image} />
@@ -12,10 +12,10 @@ const ProjectCard = props => (
     <div className="card-reveal">
       <span className="card-title grey-text text-darken-4">{props.name}
         <i className="fa fa-window-close" aria-hidden="true" /></span>
-      <p>{props.description}</p>
-      <p> <a href={props.github}><i class="fab fa-github"></i> Github</a></p>
-      <p><a href={props.link}><i class="fas fa-external-link-square-alt"></i> Check it out</a></p>
 
+      <h5 classname="links"> <a href={props.github}><i class="fab fa-github"></i> Github</a></h5>
+      <h5 classname="links"><a href={props.link}><i class="fas fa-external-link-square-alt"></i> Check it out</a></h5>
+      <p>{props.description}</p>
     </div>
   </div>
 
