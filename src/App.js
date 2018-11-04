94219dc7d07319a834d8b3220d7ad99bc9ef7b32
MCPolichette <mark@polichette.com>
2018-11-04 12:13:22 -0700

    trying to fix merge

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
         <header className="App-header">
           <ProfilePicture /><h3>Mark Polichette</h3>
         </header>
