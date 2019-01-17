70973eb8079d59c4722a13902d16e11c4644e76d
MCPolichette <mark@polichette.com>
2019-01-17 10:18:54 -0700

    minor adjustment of formatting

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -4,7 +4,8 @@ import "./ProjectCard.css";
 
 const ProjectCard = props => (
   <div className="card">
-    <h5 className=" activator card-topper">{props.name}</h5>
+    <h5 className=" activator card-topper">{props.name}
+    </h5>
     <div className="card-image waves-effect waves-block waves-light">
       <img className="activator" src={window.location + `/img/${props.image}`} alt={props.name} />
     </div>
@@ -12,13 +13,24 @@ const ProjectCard = props => (
       <span className="card-title grey-text text-darken-4">
         <i className="fas fa-times-circle closing-button" aria-hidden="true" />
         <span>
-          <a href={props.link}><i className="fas fa-external-link-square-alt"></i></a>  {props.name}
-          {props.github ? <a href={props.github}><i className="fab fa-github"></i> </a> : <a href={props.link}><i className="fas fa-external-link-square-alt"></i></a>}
+          <a href={props.link}>
+            <i className="fas fa-external-link-square-alt">
+            </i>
+          </a>  {props.name}
+          {props.github ? <a href={props.github}>
+            <i className="fab fa-github">
+            </i>
+          </a> : <a href={props.link}>
+              <i className="fas fa-external-link-square-alt">
+              </i>
+            </a>
+          }
         </span>
       </span>
 
 
-      <p>{props.description}</p>
+      <p>{props.description}
+      </p>
     </div>
   </div>
   // 
